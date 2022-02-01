bd727a0ac4519d4762c9b40b40883a79dd0186d4
17Hieng <94735928+17Hieng@users.noreply.github.com>
2022-02-01 10:50:03 +0800

    UpdatedCreate README.md

--- Triangle/Triangle.cpp
+++ Triangle/Triangle.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main()
-{
-    cout << "Please enter number of line of triangle\n";
-
+{   
+    int n,u;
+    cout << "Please enter max width of triangle\n";//Ask max width from user
+    cin >> n;
+    for (int i = 1; i <= (n * 2 - 1);i++) {//Loop until triangle completed
+        if (i < n) {
+            u = i;
+        }
+        else {
+            u = n - (i - n);
+        }
+        for (int j = 1; j <= u;j++) {//Loop until the width of triangle
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
 }
